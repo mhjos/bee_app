8d0dcb88861fad6a8ff2682c922f23655229c6bb
Maia Jøsang <josangmaia@gmail.com>
2025-10-22 14:30:17 +1000

    Changed config.py password

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:rootpassword@db/bee_db'
+    SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://bee:bee123@db/bee_db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'supersecretkey'
